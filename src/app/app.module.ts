078a141d62f40325260a3a2d488589fb3a8b3ca3
Lucho <lucianomartin2015nqn@gmail.com>
2022-07-11 23:08:54 -0300

    Crea Layout stack

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './componente/cabecera/cabecera.component';
 import { AcercaDeComponent } from './componente/acerca-de/acerca-de.component';
+import { StackComponent } from './componente/stack/stack.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    StackComponent
   ],
   imports: [
     BrowserModule
