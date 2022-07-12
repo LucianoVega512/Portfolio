1ea9b70738656bb7f40d0a9cdf7a907a1aeab695
Lucho <lucianomartin2015nqn@gmail.com>
2022-07-11 21:00:19 -0300

    Agrega banner en cabecera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './componente/cabecera/cabecera.component';
+import { AcercaDeComponent } from './componente/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule
