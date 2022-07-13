7bb7f700202bb6a9f83f01c961a68f5fca82dcf1
Lucho <lucianomartin2015nqn@gmail.com>
2022-07-13 11:27:53 -0300

    Crea layout proyectos, inicia funcionalidad con carousel

--- src/app/componente/proyectos/proyectos.component.ts
+++ src/app/componente/proyectos/proyectos.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponent implements OnInit {
 
+  imagenes_url: string[] = ["./assets/spring.png", "./assets/mysql.png", "./assets/programacion.png"];
+  imagen_actual: string = "./assets/spring.png";
+  indice_imagen: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  siguiente() {
+    if (this.indice_imagen < 2) {
+      this.imagen_actual = this.imagenes_url[++this.indice_imagen];
+    }
+  }
+
+  anterior() {
+    if (this.indice_imagen > 0) {
+      this.imagen_actual = this.imagenes_url[--this.indice_imagen];
+    }
   }
 
 }
