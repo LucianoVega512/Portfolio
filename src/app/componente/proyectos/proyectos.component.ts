f952b05f66c238b00695dda43ba7e6833d384c28
Lucho <lucianomartin2015nqn@gmail.com>
2022-07-13 15:38:22 -0300

    Crea carouesel y da funcionalidad a los botones siguiente y anterior, agrega descripcion de proyecto

--- src/app/componente/proyectos/proyectos.component.ts
+++ src/app/componente/proyectos/proyectos.component.ts
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponent implements OnInit {
 
-  imagenes_url: string[] = ["./assets/spring.png", "./assets/mysql.png", "./assets/programacion.png"];
+  // imagenes_url:[string, string] = ["jashnd", "sjdfjk"]
+
+  proyectos: { url: string, descripcion: string }[] = [{url:"./assets/spring.png", descripcion:"descripcion 1"}, 
+    {url:"./assets/mysql.png", descripcion:"descripcion 2"}, {url:"./assets/programacion.png", descripcion:"descripcion 3"}];
+
+  // [{"./assets/spring.png", "Esta es una"}, {"./assets/mysql.png", "sdfd"}, {"./assets/programacion.png"}];
   imagen_actual: string = "./assets/spring.png";
+  imagen_descripcion: string = "descripcion 1";
   indice_imagen: number = 0;
 
   constructor() { }
@@ -18,13 +24,15 @@ export class ProyectosComponent implements OnInit {
 
   siguiente() {
     if (this.indice_imagen < 2) {
-      this.imagen_actual = this.imagenes_url[++this.indice_imagen];
+      this.imagen_actual = this.proyectos[++this.indice_imagen].url;
+      this.imagen_descripcion = this.proyectos[this.indice_imagen].descripcion;
     }
   }
 
   anterior() {
     if (this.indice_imagen > 0) {
-      this.imagen_actual = this.imagenes_url[--this.indice_imagen];
+      this.imagen_actual = this.proyectos[--this.indice_imagen].url;
+      this.imagen_descripcion = this.proyectos[this.indice_imagen].descripcion;
     }
   }
 
