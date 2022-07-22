784d7d532144529c93dd1e32b722297aa951fba1
Lucho <lucianomartin2015nqn@gmail.com>
2022-07-21 22:38:06 -0300

    Modifica stack y agrega chips eliminables

--- src/app/componente/stack/stack.component.ts
+++ src/app/componente/stack/stack.component.ts
@@ -9,15 +9,23 @@ export class StackComponent implements OnInit {
 
   mostrarVentanaStack: boolean;
 
+  tarjetas: { claseTarjeta: string, urlImagen: string, descripcion: string }[] = [
+    {claseTarjeta: "contenedor-tarjeta1", urlImagen: "./assets/angular.png", descripcion: "Programacion front-end con el framework"},
+    {claseTarjeta: "contenedor-tarjeta2", urlImagen: "./assets/spring.png", descripcion: "Programacion front-end con el framework"},
+    {claseTarjeta: "contenedor-tarjeta3", urlImagen: "./assets/mysql.png", descripcion: "Programacion front-end con el framework"}
+  ];
+
+  chips: string[] = ["Prime NG", "Git/GitHub", "Scrum", "Ng2-Charts", "Font Awesome", "CSS-Grid", "JWT", "Java 11"];
+
   constructor() {
     this.mostrarVentanaStack = false;
   }
 
-  cerrarVentana(){
+  cerrarVentana() {
     this.mostrarVentanaStack = false;
   }
 
-  abrirVentanaEdicion(){
+  abrirVentanaEdicion() {
     this.mostrarVentanaStack = true;
   }
 
